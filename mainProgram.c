26aca97cf06c31dac4402c5a363c0782d0cf1618
MarlyB5 <marlybah051205@gmail.com>
2026-05-19 13:35:47 +0100

    Commit programs

---
+++ mainProgram.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "SAVEBOARD_HEADER.h"
+
+
+
+//STRUCTS -----------------------
+
+int main () {
+
+    
+
+
+
+
+    
+}
---
+++ quitProgram.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#define QUITFUNCTION_H
+
+
+void quitProgram (){
+
+    printf ("Quitting program... ˙◠˙"); 
+    exit (0);
+
+}
